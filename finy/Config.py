94c9fa2a543b70a4565762f3ecdddcd88d271547
adiel <amatuf@tremorvideo.com>
2021-12-18 21:26:37 +0200

    update heroku

--- finy/Config.py
+++ finy/Config.py
@@ -7,6 +7,8 @@ class TelegramBotConfig:
     bot_token = os.getenv("TOKEN")  # Telegram Bot API Key
     # chat_id = os.getenv("CHAT_ID")  # Telegram Chat ID
     bot_user_name = os.getenv("USERNAME")
+    send_message_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    set_webhook_url = f"https://api.telegram.org/bot{bot_token}/setWebhook"
 
 
 @dataclass
